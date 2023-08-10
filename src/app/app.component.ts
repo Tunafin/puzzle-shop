c237c5097db0ad55c41c94ebba0cc6eeb3814446
tunafin <tunaweichi@gmail.com>
2023-08-10 22:11:10 +0800

    adjust favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'puzzle-shop';
+  private _title = 'Puzzle Shop';
+  public get title() {
+    return this._title;
+  }
+  public set title(value) {
+    if (this._title != value) {
+      this._title = value;
+      this.titleService.setTitle(this._title);
+
+    }
+  }
+
+  private isAdminPage = false;
+
+  constructor(private router: Router, private titleService: Title) {
+    this.router.events
+      .subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          const isAdminPage = event.url.startsWith('/admin');
+          if (this.isAdminPage != isAdminPage) {
+            this.isAdminPage = isAdminPage
+            this.title = isAdminPage ? 'Puzzle Admin' : 'Puzzle Shop';
+
+            const favicon: HTMLLinkElement = document.querySelector('#favicon')!;
+            favicon.href = isAdminPage ? 'assets/favicon_ng.ico' : 'assets/favicon.ico';
+            console.log(favicon);
+
+          }
+        }
+      });
+  }
 }
